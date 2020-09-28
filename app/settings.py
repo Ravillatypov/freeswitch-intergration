65ddacc26d0ebb02889789939c8229ab6f9ab6df
Ravil Latypov <ravillatypov12@gmail.com>
2020-09-28 12:21:10 +0300

    optimize settings

--- app/settings.py
+++ app/settings.py
@@ -17,9 +17,9 @@ __dev = 'dev.' if DEV else ''
 TELEPHONY_URL = f'https://telephony.{__dev}moydomonline.ru/api/v1/telehony/freeswitch/call_events/'
 
 MQ_DSN = env.str('MQ_DSN')
-MQ_EVENTS_QUEUE_NAME = env.str('MQ_EVENTS_QUEUE_NAME')
-MQ_CONVERTER_QUEUE_NAME = env.str('MQ_CONVERTER_QUEUE_NAME')
-MQ_UPLOADS_QUEUE_NAME = env.str('MQ_UPLOADS_QUEUE_NAME')
+MQ_EVENTS_QUEUE_NAME = env.str('MQ_EVENTS_QUEUE_NAME', default='fs_events')
+MQ_CONVERTER_QUEUE_NAME = env.str('MQ_CONVERTER_QUEUE_NAME', default='convert_tasks')
+MQ_UPLOADS_QUEUE_NAME = env.str('MQ_UPLOADS_QUEUE_NAME', default='upload_tasks')
 
 S3_FOLDER_NAME = env.str('S3_FOLDER_NAME', default='audios')
 S3_BUCKET_NAME = env.str('S3_BUCKET_NAME', default='mdo-appeals')
@@ -27,7 +27,3 @@ S3_BUCKET_NAME = env.str('S3_BUCKET_NAME', default='mdo-appeals')
 DATA_PATH = env.str('DATA_PATH', default='/data')
 
 LOG_LEVEL = env.str('LOG_LEVEL', default='WARNING').upper()
-
-PASSWORD = env.str('PASSWORD', default='ClueCon')
-IP = env.str('IP', default='127.0.0.1')
-PORT = env.int('PORT', default=8021)
